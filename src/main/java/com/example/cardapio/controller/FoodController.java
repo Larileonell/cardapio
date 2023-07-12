d7ccec3a845bb03d30936dc83b28bc482c72e65f
Allana Larissa <larissaleonel98@gmail.com>
2023-07-12 17:28:07 -0300

    ADD FOOD REPOSITORY AND RESPONSEDTO

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,14 +1,23 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
 @RequestMapping("food")
 public class FoodController {
+    @Autowired
+    private FoodRepository repository;
     @GetMapping
-    public void getAll(){
-
+    public List<FoodResponseDTO> getAll(){
+        List<Food> foodList = repository.findAll().stream().map(new FoodResponseDTO());
+        return foodList;
     }
 }
